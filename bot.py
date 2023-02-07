eab5dce3b208a5c38fd7f84b9642a4423a0431aa
EliteB0Y <itseliteboy@gmail.com>
2023-02-07 08:55:55 +0530

    added 2 shard raids

--- bot.py
+++ bot.py
@@ -22,8 +22,11 @@ class MyBot(commands.Bot):
     inviteurl = ""
     boxrateconfig = {"base": 1, "unbase": 0.8, "other": 5}
     raids_config = {'category_id': 1043874022552780880, 
-                    'channel_id': 1071627979110760499, 
-                    'doable_raids': ['articuno', 'mew', 'mewtwo', 'celebi', 'deoxys', 'uxie', 'mesprit', 'azelf', 'cresselia', 'heatran', 'phione', 'manaphy', 'shaymin', 'virizion', 'genesect', 'keldeo', 'diancie', 'hoopa', 'tapu lele', 'lunala', 'nihilego', 'pheromosa', 'kartana', 'necrozma', 'poipole', 'naganadel', 'stakataka', 'blacephalon', 'xurkitree', 'melmetal', 'zarude', 'calyrex', 'enamorus', 'wo-chien', 'chien-pao', 'chi-yu', 'rapid strike style urshifu', 'gigantamax rapid strike style urshifu', 'single strike urshifu']}    
+                    '10_shards_raids_ch': 1071627979110760499,
+                    '2_shards_raids_ch': 1072353312164298833,
+                    '10_shards_raids': ['articuno', 'mew', 'mewtwo', 'celebi', 'deoxys', 'uxie', 'mesprit', 'azelf', 'cresselia', 'heatran', 'phione', 'manaphy', 'shaymin', 'virizion', 'genesect', 'keldeo', 'diancie', 'hoopa', 'tapu lele', 'lunala', 'nihilego', 'pheromosa', 'kartana', 'necrozma', 'poipole', 'naganadel', 'stakataka', 'blacephalon', 'xurkitree', 'melmetal', 'zarude', 'calyrex', 'enamorus', 'wo-chien', 'chien-pao', 'chi-yu', 'rapid strike style urshifu', 'gigantamax rapid strike style urshifu', 'single strike urshifu'],
+                    '2_shards_raids': ['alolan rattata', 'alolan raticate', 'alolan sandshrew', 'alolan vulpix', 'alolan ninetales', 'alolan diglett', 'alolan meowth', 'alolan persian', 'alolan geodude', 'galarian meowth', 'galarian ponyta', 'galarian slowpoke', 'galarian zigzagoon', 'galarian linoone', 'galarian darumaka', 'hisuian growlithe', 'hisuian voltorb', 'hisuian sneasel', 'hisuian lilligant', 'hisuian decidueye']
+                    }    
     
     def __init__(self, command_prefix, intents):
         super().__init__(command_prefix=command_prefix, intents=intents)
@@ -88,10 +91,17 @@ async def on_message(message):
             if "Raid Announcement" in embed.title:
                 description = embed.description
                 raid_boss = description.splitlines()[1].split(":**", maxsplit=1)[1].lower().strip()
-                if raid_boss in client.raids_config["doable_raids"]:
+
+                if raid_boss in client.raids_config["10_shards_raids"]:
                     embed = discord.Embed(title= "⚔️ Raid Announcement ⚔️", description=description, url = message.jump_url, color = discord.Color.teal())
                     embed.add_field(name="Channel:", value=f"{message.channel.mention}")
-                    channel_id = client.get_channel(client.raids_config["channel_id"])
+                    channel_id = client.get_channel(client.raids_config["10_shards_raids_ch"])
+                    await channel_id.send(embed=embed)
+
+                elif raid_boss in client.raids_config["2_shards_raids"]:
+                    embed = discord.Embed(title= "⚔️ Raid Announcement ⚔️", description=description, url = message.jump_url, color = discord.Color.yellow())
+                    embed.add_field(name="Channel:", value=f"{message.channel.mention}")
+                    channel_id = client.get_channel(client.raids_config["2_shards_raids_ch"])
                     await channel_id.send(embed=embed)
 
     if message.author.bot or message.author.id in client.blocklist:
